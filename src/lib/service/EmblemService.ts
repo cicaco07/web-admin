c026a9ecd44c6da96dfd69e886291624ab59f8c4
Aryo <chiefdevica@gmail.com>
2025-08-06 23:58:58 +0700

    feat: enhance emblem management with edit and delete functionalities

--- src/lib/service/EmblemService.ts
+++ src/lib/service/EmblemService.ts
@@ -1,9 +1,11 @@
-import { alertError, alertSuccess } from "../alert";
-import { useCreateEmblem } from "../api/EmblemApi";
+import { alertConfirm, alertError, alertSuccess } from "../alert";
+import { useCreateEmblem, useDeleteEmblem, useUpdateEmblem } from "../api/EmblemApi";
 
 export const useEmblemService = (refetch: () => Promise<any>) => {
 
   const { createEmblem } = useCreateEmblem();
+  const { updateEmblem } = useUpdateEmblem();
+  const { deleteEmblem } = useDeleteEmblem();
 
   const handleAddEmblem = async (emblemForm: any) => {
     try {
@@ -21,7 +23,45 @@ export const useEmblemService = (refetch: () => Promise<any>) => {
     }
   };
 
+  const handleEditEmblem = async (editEmblem: any) => {
+    try {
+      const input = {
+        name: editEmblem.name,
+        type: editEmblem.type,
+        icon: editEmblem.icon,
+        benefit: editEmblem.benefit,
+        description: editEmblem.description,
+        cooldown: Number(editEmblem.cooldown),
+      };
+      await updateEmblem({ id: editEmblem._id, input });
+      await refetch();
+      alertSuccess('Emblem berhasil diupdate!');
+      (window as any).bootstrap?.Modal?.getOrCreateInstance(document.getElementById('edit-emblem'))?.hide();
+    } catch (error) {
+      alertError('Gagal mengupdate emblem.');
+      console.error(error);
+      throw error;
+    }
+  };
+
+  const handleDeleteEmblem = async (id: string) => {
+    const confirm = await alertConfirm('Yakin ingin menghapus emblem ini? Data emblem akan dihapus permanen!');
+    if (confirm == true) {
+      try {
+        await deleteEmblem({ id });
+        await refetch();
+        alertSuccess('Emblem berhasil dihapus!');
+      } catch (error) {
+        alertError('Gagal menghapus emblem.');
+        console.error(error);
+        throw error;
+      }
+    }
+  };
+
   return {
     handleAddEmblem,
+    handleDeleteEmblem,
+    handleEditEmblem
   };
 };
