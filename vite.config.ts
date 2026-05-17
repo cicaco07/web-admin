a8f10077a5e3f5054b75c137405b253820536d33
Aryo <chiefdevica@gmail.com>
2026-05-17 16:06:05 +0700

    fix: vite error build in production

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue-vendor': ['vue', 'vue-router'],
+          'apollo-vendor': ['@apollo/client', '@vue/apollo-composable', 'graphql', 'graphql-tag'],
+          'xlsx-vendor': ['xlsx'],
+          'ui-vendor': ['sweetalert2', 'vuedraggable', '@vueuse/core'],
+        },
+      },
+    },
+  },
 })
